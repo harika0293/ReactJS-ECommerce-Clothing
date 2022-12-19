1bd090fcae45fc37720dac6252c9f1edd966caee
Harika Rachamalla <harika.0293@gmail.com>
2022-12-19 20:24:05 +0530

    Updates

---
+++ precache-manifest.b5a8750aa5e68801bb727cf2feb2817a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28d2261f4ef27ee583e2576d90095ec",
+    "url": "/ReactJS-ECommerce-Clothing/index.html"
+  },
+  {
+    "revision": "6cbf7eb5b85a579ba867",
+    "url": "/ReactJS-ECommerce-Clothing/static/css/main.c439ac71.chunk.css"
+  },
+  {
+    "revision": "c5c1fd242cd258f09a3a",
+    "url": "/ReactJS-ECommerce-Clothing/static/js/2.d384f59a.chunk.js"
+  },
+  {
+    "revision": "6cbf7eb5b85a579ba867",
+    "url": "/ReactJS-ECommerce-Clothing/static/js/main.c0f58a13.chunk.js"
+  },
+  {
+    "revision": "846ca423527c8a896ea9",
+    "url": "/ReactJS-ECommerce-Clothing/static/js/runtime-main.29535881.js"
+  }
+]);
